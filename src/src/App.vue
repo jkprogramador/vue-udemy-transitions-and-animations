c14122c988e494f2aaff24fa32f0022f91467e19
Jack Lee <jkprogramador@gmail.com>
2022-04-05 11:45:56 -0300

    Animating with CSS Transitions

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,7 +1,43 @@
-<template></template>
+<template>
+    <div class="row my-4">
+        <div class="col">
+            <button
+                @click="flag = !flag"
+                class="btn btn-success btn-lg"
+                type="button"
+            >
+                Toggle
+            </button>
+
+            <transition name="fade">
+                <h2 v-if="flag" class="display-2 my-3">Hello World!</h2>
+            </transition>
+        </div>
+    </div>
+</template>
 
 <script>
 export default {
     name: "App",
+    data() {
+        return {
+            flag: false,
+        };
+    },
 };
 </script>
+
+<style>
+.fade-enter-from {
+    opacity: 0;
+}
+
+.fade-enter-active {
+    transition: all 0.25s linear;
+}
+
+.fade-leave-to {
+    transition: all 0.25s linear;
+    opacity: 0;
+}
+</style>
